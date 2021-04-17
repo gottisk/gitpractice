c2e10599282b4064b84b675f3ca54df86104327c
root <root@skgottigeregmai.tvbw0tab4pze3azka0sarqhese.gx.internal.cloudapp.net>
2021-04-17 18:34:04 +0000

    changes added to child branch

--- Hello.java
+++ Hello.java
@@ -3,7 +3,10 @@ class Hello
 publc static void main(String args[])
 {
   System.out.println("this is child file ");
+<<<<<<< HEAD
   System.out.println("adding new content to the chid branch ");
+=======
+>>>>>>> fe2ccd3... added
 }
 }
 
