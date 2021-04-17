f7f1319e0af0b890ef90962efe5b3a354140438c
root <root@skgottigeregmai.tvbw0tab4pze3azka0sarqhese.gx.internal.cloudapp.net>
2021-04-17 20:49:33 +0000

    made more changes

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ publc static void main(String args[])
   System.out.println("this is child file ");
 <<<<<<< HEAD
   System.out.println("adding new content to the chid branch ");
+  System.out.println("add new changes to child again");
 =======
 >>>>>>> fe2ccd3... added
 }
