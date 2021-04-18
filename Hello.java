e4d226d13d6c7175f78e7309785a08674223d74a
root <root@skgottigeregmai.tvbw0tab4pze3azka0sarqhese.gx.internal.cloudapp.net>
2021-04-18 10:40:56 +0000

    changes

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
-class Hello
+nclass Hello
 {
 publc static void main(String args[])
 {
-  System.out.println("this is child file "):
   System.out.println("adding new content to the chid branch ");
   System.out.println("add new changes to child again");
 
